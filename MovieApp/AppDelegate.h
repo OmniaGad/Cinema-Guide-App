6252606a0a11b14a6c4b7afda938a93fbed96518
omnia <omnia@learniosfreecoms-mac.local>
2018-02-28 21:17:02 +0200

    Initial Commit

---
+++ MovieApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieApp
+//
+//  Created by omnia on 2/28/18.
+//  Copyright © 2018 omnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MovieApp
+//
+//  Created by omnia on 2/28/18.
+//  Copyright © 2018 omnia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
